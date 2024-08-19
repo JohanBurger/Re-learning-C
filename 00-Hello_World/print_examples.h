68d0288e14e71bb260035526866b447a77c17027
Johan <jdpburger@gmail.com>
2024-08-19 18:33:46 +0200

    Getting started with C again.

---
+++ 00-Hello_World/print_examples.h
@@ -0,0 +1,9 @@
+#ifndef PRINT_EXAMPLES_H
+#define PRINT_EXAMPLES_H
+
+void print_simple_greeting();
+void print_greeting(char* first_name, char* last_name, char learning, int age, float some_float);
+void print_asterisks(int width, int height);
+void print_numbers_asterisks(int width, int height);
+
+#endif //PRINT_EXAMPLES_H
