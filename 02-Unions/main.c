b1a2806f4346824ddf845eedf47e385b7759eed0
Johan <jdpburger@gmail.com>
2024-09-04 19:31:14 +0200

    Clean up, comments

--- 02-Unions/main.c
+++ 02-Unions/main.c
@@ -8,15 +8,18 @@ union info {
 
 int main(void) {
     union info myInfo;
+
+    // As expected
     myInfo.age = 20;
-
     printf("Age: %d\n", myInfo.age);
 
+    // As expected
     strcpy_s(myInfo.firstName, sizeof(myInfo.firstName), "Johan");
-    // myInfo.firstName = "Johan";
     printf("First Name: %s\n", myInfo.firstName);
+    // Age has been overwritten by the first 4 bytes of the string, garbage.
     printf("Age: %d\n", myInfo.age);
 
+    // They're all the same location
     printf("Location of myInfo: %p\n", &myInfo);
     printf("Location of myInfo.age: %p\n", &myInfo.age);
     printf("Location of myInfo.firstName: %p\n", &myInfo.firstName);
