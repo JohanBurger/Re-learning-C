c3be40f733d6d4a5d39b0d77cada8c1281820924
Johan <jdpburger@gmail.com>
2024-12-10 07:45:03 +0100

    Implement generic swap also

--- 06-Pointers/pointer_operations.c
+++ 06-Pointers/pointer_operations.c
@@ -1,5 +1,17 @@
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// void swap(int* a, int* b) {
+//     int temp = *a;
+//     *a = *b;
+//     *b = temp;
+// }
+
+void swap(void* a, void* b, const size_t size) {
+    void* temp = malloc(size);
+    memcpy(temp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, temp, size);
+    free(temp);
 }
