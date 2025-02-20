1f108c8120f5a36df51d750456791f1c26588f07
Johan <jdpburger@gmail.com>
2025-02-20 21:29:17 +0100

    Some more playing around with arrays.
    Also pass-by-reference

--- 06-Pointers/pointer_operations.h
+++ 06-Pointers/pointer_operations.h
@@ -9,5 +9,9 @@ int sum(const int* arr, const size_t size);
 bool has_element(const int* arr, const size_t size, const int element);
 int get_max(const int* arr, const size_t size);
 double get_average(const int* arr, const size_t size);
+void input_array(const int* array, const size_t size);
+void print_array();
+void get_min_max(const int* arr, const size_t size, int* min, int* max);
+void update_average(const int a, const int b, float* average);
 
 #endif //POINTEROPERATIONS_H
